c55cc76580ca2a22cb90b0b5121752bc02f5152d
dwarncke <daniel.warncke@gmail.com>
2019-09-09 01:21:51 +0000

    image blur 2 and 3

---
+++ image_blur2.rb
@@ -0,0 +1,55 @@
+class Image
+
+  def initialize (data)
+    @data = data
+  end
+
+  def locate_ones
+    ones = []
+    @data.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+        if item == 1
+          ones << [row_number, col_number]
+        end
+      end
+    end
+    ones
+  end
+
+  def image_blur
+    ones = locate_ones
+      @data.each_with_index do |row, row_number|
+        row.each_with_index do |item, col_number|
+          ones.each do |found_row_number, found_col_number|
+
+            if row_number == found_row_number && col_number == found_col_number
+              @data[row_number -1][col_number] = 1 unless row_number == 0 
+              @data[row_number +1][col_number] = 1 unless row_number >= @data.length - 1 
+              @data[row_number][col_number -1] = 1 unless col_number == 0 
+              @data[row_number][col_number +1] = 1 unless col_number >= row.length - 1 
+            end
+          end
+        end
+      end
+  end
+
+  def output_image
+    @data.each do |data|
+      puts data.join
+    end
+  end
+end
+
+image = Image.new([
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 1, 0, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0 ,0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0]
+])
+
+image.output_image
+puts
+image.image_blur
+image.output_image
